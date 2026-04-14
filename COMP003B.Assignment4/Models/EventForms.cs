22a4af393f95454948b7017cdc416fba8cf83310
samian <sam.iannazzo@yahoo.com>
2026-04-14 16:29:33 -0700

    added required field stringlength 100 min 3 for public string FullName in Model EventForms

--- COMP003B.Assignment4/Models/EventForms.cs
+++ COMP003B.Assignment4/Models/EventForms.cs
@@ -4,6 +4,12 @@ namespace COMP003B.Assignment4.Models
 {
     public class EventForms
     {
-       
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+
+        public string FullName { get; set; }
+
     }
 }
