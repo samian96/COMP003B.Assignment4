3958ee2608ce366ebee07390b738c0b26bda7d3e
samian <sam.iannazzo@yahoo.com>
2026-04-14 16:41:58 -0700

    Created controller for Event / added HttpGet return view

---
+++ COMP003B.Assignment4/Controllers/EventController.cs
@@ -0,0 +1,17 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    [Route("event")]
+    public class EventController : Controller
+    {
+        [HttpGet] 
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
