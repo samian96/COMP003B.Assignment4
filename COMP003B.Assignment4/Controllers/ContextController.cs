64dfbd9f76767117ad639c00550d11b9b6497147
samian <sam.iannazzo@yahoo.com>
2026-04-15 00:01:20 -0700

    fixed naming error

--- COMP003B.Assignment4/Controllers/ContextController.cs
+++ COMP003B.Assignment4/Controllers/ContextController.cs
@@ -1,21 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using COMP003B.Assignment4.Models;
+using System.Diagnostics.Tracing;
 
 namespace COMP003B.Assignment4.Controllers
 {
     public class ContextController : Controller
     {
-    public IActionResult Index()
+        public IActionResult Index()
         {
             var context = new List<Context>
             {
-                new Context {Id = 1, Title = "This is a test title", Description = "This is a test description" }
+                new Context {Id = 1, Title = "This is a test title", Description = "This is a test description" },
+
+                new Context {Id = 2, Title = "Second test for a title", Description = "This is a test description"}
             };
 
             return View(context);
         }
 
-        public IActionResult Details(int id)
+        public IActionResult Context(int id)
         {
             var context = new Context { Id = id, Title = $"Context {id}", Description = "Detailed content coming soon" };
             return View(context);
