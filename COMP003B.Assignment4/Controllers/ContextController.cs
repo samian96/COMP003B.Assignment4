ee1f4783dfb1c4cee92d869606b70f8ed368d525
samian <sam.iannazzo@yahoo.com>
2026-04-14 22:52:03 -0700

    added var context to context controller

--- COMP003B.Assignment4/Controllers/ContextController.cs
+++ COMP003B.Assignment4/Controllers/ContextController.cs
@@ -5,6 +5,14 @@ namespace COMP003B.Assignment4.Controllers
 {
     public class ContextController : Controller
     {
-    
+    public IActionResult Context()
+        {
+            var context = new List<Context>
+            {
+                new Context {Id = 1, Title = "This is a test title", Description = "This is a test description" }
+            };
+
+            return View(context);
+        }
     }
 }
