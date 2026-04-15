73abd2ef72a4e4b4fbb0a520e63a1b5a92180a38
samian <sam.iannazzo@yahoo.com>
2026-04-14 22:47:32 -0700

    created contextcontroller

---
+++ COMP003B.Assignment4/Controllers/ContextController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment4.Models;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    public class ContextController : Controller
+    {
+    
+    }
+}
