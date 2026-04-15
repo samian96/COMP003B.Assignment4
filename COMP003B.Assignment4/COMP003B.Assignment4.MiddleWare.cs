e2d5864543586980b98e47a9af39c23c7acf421d
samian <sam.iannazzo@yahoo.com>
2026-04-14 21:02:29 -0700

    added comment to COMP004Middleware

--- COMP003B.Assignment4/COMP003B.Assignment4.MiddleWare.cs
+++ COMP003B.Assignment4/COMP003B.Assignment4.MiddleWare.cs
@@ -11,6 +11,7 @@
 
         public async Task Invoke(HttpContext context)
         {
+            // displays in the console app when user input happens shows the request and response 
             Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
                 await _next(context);
             Console.WriteLine($"[Respond] {context.Response.StatusCode}");
