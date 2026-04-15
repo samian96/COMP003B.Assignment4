5f676c20a2d7e2e7b262d439e797e76f1c38e9c0
samian <sam.iannazzo@yahoo.com>
2026-04-14 20:08:14 -0700

    created success view / added httppost with IsValid

--- COMP003B.Assignment4/Controllers/EventController.cs
+++ COMP003B.Assignment4/Controllers/EventController.cs
@@ -1,17 +1,37 @@
 ﻿using COMP003B.Assignment4.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Tracing;
 
 namespace COMP003B.Assignment4.Controllers
 {
     [Route("event")]
     public class EventController : Controller
     {
-        [HttpGet] 
+        [HttpGet("orderform")] 
 
         public IActionResult OrderForm()
         {
             return View();
         }
 
+        [HttpPost("orderform")]
+
+        public IActionResult OrderForm([FromForm] OrderForm orderform)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(orderform);
+            }
+
+            return RedirectToAction("Success", orderform);
+        }
+
+        [Route("success")]
+
+        public IActionResult Success(OrderForm orderform)
+        {
+            return View(orderform);
+        }
     }
+
 }
