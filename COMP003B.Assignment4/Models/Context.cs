8e3a5a5a3dc16f2b7865aa115c57e7a5de60e40e
samian <sam.iannazzo@yahoo.com>
2026-04-14 22:46:11 -0700

    Added required fields to context model

--- COMP003B.Assignment4/Models/Context.cs
+++ COMP003B.Assignment4/Models/Context.cs
@@ -4,5 +4,18 @@ namespace COMP003B.Assignment4.Models
 {
     public class Context
     {
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(100)]
+        
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(300)]
+
+        public string Description { get; set; }
+
     }
 }
