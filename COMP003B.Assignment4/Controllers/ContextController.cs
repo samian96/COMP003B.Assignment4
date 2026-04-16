b0cb9f9b4b445341050e032e2afb6e9280c3871b
samian <sam.iannazzo@yahoo.com>
2026-04-15 20:01:52 -0700

    added route to context controller / created new view for context/ index2

--- COMP003B.Assignment4/Controllers/ContextController.cs
+++ COMP003B.Assignment4/Controllers/ContextController.cs
@@ -4,15 +4,19 @@ using System.Diagnostics.Tracing;
 
 namespace COMP003B.Assignment4.Controllers
 {
+    [Route("context")]
     public class ContextController : Controller
     {
+        [Route("index")]
         public IActionResult Index()
         {
             var context = new List<Context>
             {
                 new Context {Id = 1, Title = "This is a test title", Description = "This is a test description" },
 
-                new Context {Id = 2, Title = "Second test for a title", Description = "This is a test description"}
+                new Context {Id = 2, Title = "Second test for a title", Description = "This is a test description"},
+
+                new Context {Id = 3, Title = "Second test for a title", Description = "This is a test description"}
             };
 
             return View(context);
